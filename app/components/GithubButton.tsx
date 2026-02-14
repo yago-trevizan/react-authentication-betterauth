acecb77107b58ccb26d4ee638442b1fa1a545c72
Yago Trevizan de Araujo <yago.ta@hotmail.com>
2026-02-14 16:22:37 -0300

    refactor: change color of `GithubButton` component

--- app/components/GithubButton.tsx
+++ app/components/GithubButton.tsx
@@ -12,7 +12,7 @@ export default function GithubButton() {
   return (
     <button
       onClick={signIn}
-      className="bg-blue-100 w-full uppercase active:bg-blue-200 py-1 rounded font-semibold text-blue-700 cursor-pointer border-2 border-blue-700"
+      className="bg-red-100 w-full uppercase active:bg-red-200 py-1 rounded font-semibold text-red-700 cursor-pointer border-2 border-red-700"
     >
       GitHub
     </button>
